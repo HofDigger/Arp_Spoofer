6f2d23ba322ef0c4720022a24bd08b1a49d8b36c
HofDigger <illiakrasney@gmail.com>
2024-11-08 21:01:42 +0200

    First

---
+++ temp.py
@@ -0,0 +1,16 @@
+import scapy.all as scapy
+import os
+
+
+
+def get_mac(ip_target):
+    return 's'
+
+
+
+ip_target = '10.100.102.55'
+default_gateaway = '10.100.102.1'
+ip_src = '10.100.102.39'
+
+
+
